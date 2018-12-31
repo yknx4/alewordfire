437d0e5d33958dcd084ccb5cdc45fc0188de3f39
[CircleCI] Ale Figueroa <afigueroa@goamv.com>
2018-12-31 14:40:18 -0600

    Update scripts

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :alewordfire, AlewordfireWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet6, port: 80],
   url: [host: "www.ale.world", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   load_from_system_env: true,
