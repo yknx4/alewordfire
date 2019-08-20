428dacb09c1c019f53348e40be7a53576eddbd4f
Ale Figueroa <afigueroa@goamv.com>
2019-08-20 20:39:07 +1000

    Fix some more issues

--- lib/alewordfire_web/endpoint.ex
+++ lib/alewordfire_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule AlewordfireWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :alewordfire,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
